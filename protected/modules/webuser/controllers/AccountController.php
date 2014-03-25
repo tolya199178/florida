eade8e1140e527108d7599185012d77ea455869d
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-03-26 01:59:01 +0200

    Save point for usermodel bugfix and cater for register scenario

---
+++ protected/modules/webuser/controllers/AccountController.php
@@ -0,0 +1,164 @@
+<?php
+
+/**
+ * Account Controller interface for the Frontend (Public) User Module
+ */
+
+
+/**
+ * AccountController is a class to provide access to controller actions for general
+ * ..processing of user account actions. The contriller action interfaces
+ * ...'directly' with the Client, and must therefore be responsible for input
+ * ...processing and response handling.
+ *
+ * Usage:
+ * ...Typical usage is from a web browser, by means of a URL
+ * ...
+ * ...   http://application.domain/index.php?/webuser/account/login/attribute1/parameter1/.../attribute-n/parameter-n/
+ * ...eg.
+ * ...   http://mydomain/index.php?/webuser/account/login/name/toms-diner/
+ * ...
+ * ...The 'action' in the request is converted to invoke the actionAction() action
+ * ...eg. /webuser/account/login/name/toms/ will invoke ProfileController::actionShow()
+ * ...(case is significant)
+ * ...Additional parameters after the action are passed as $_GET pairs
+ * ...eg. /webuser/account/login/name/toms/ will pass $_GET['name'] = 'toms'
+ *
+ * @package   Controllers
+ * @author    Pradesh <pradesh@datacraft.co.za>
+ * @copyright 2014 florida.com
+ * @package Controllers
+ * @version 1.0
+ */
+
+class AccountController extends Controller
+{
+    
+    
+    /**
+     * @var string imagesDirPath Directory where Business images will be stored
+     * @access private
+     */
+    private $imagesDirPath;
+    
+    /**
+     * @var string imagesDirPath Directory where Business image thumbnails will be stored
+     * @access private
+     */
+    private $thumbnailsDirPath;
+    
+    /**
+     * @var string thumbnailWidth thumbnail width
+     * @access private
+     */
+    private $thumbnailWidth     = 100;
+    /**
+     * @var string thumbnailWidth thumbnail width
+     * @access private
+     */
+    private $thumbnailHeight    = 100;
+    
+    /**
+     * Controller initailisation routines to set up the controller
+     *
+     * @param <none> <none>
+     *
+     * @return array action filters
+     * @access public
+     */
+    public function init()
+    {
+        $this->imagesDirPath        = Yii::getPathOfAlias('webroot').DIRECTORY_SEPARATOR.'/uploads/images/business';
+        $this->thumbnailsDirPath    = Yii::getPathOfAlias('webroot').DIRECTORY_SEPARATOR.'/uploads/images/business/thumbnails';
+    
+        /*
+         *     Small-s- 100px(width)
+        *     Medum-m- 240px(width)
+        *     Large-l- 600px(width)
+        */
+    }
+    
+    /**
+     * Register a new user
+     *
+     * @param <none> <none>
+     *
+     * @return <none> <none>
+     * @access public
+     */
+	public function actionRegister()
+	{
+
+		$formModel = new ProfileForm('register');
+		
+
+		if (isset($_POST['ajax']) && $_POST['ajax']==='profile-form') {
+			echo CActiveForm::validate(array($model));
+			Yii::app()->end();
+		}
+		
+		if (isset($_POST['ProfileForm'])) {
+		    
+		    // /////////////////////////////////////////////////////////////////
+		    // Create a new user entry
+		    // /////////////////////////////////////////////////////////////////
+		    $userModel = new User;
+		    
+		    // Copy form details
+			$userModel->setAttributes($_POST['ProfileForm']);
+			$formModel->setAttributes($_POST['ProfileForm']);
+			
+			// Add additional fields
+			$userModel->password             = 'abcd';
+			$userModel->created_by           = 1;
+			$userModel->user_name            = $userModel->email;
+			$userModel->status               = 'inactive';
+			$userModel->activation_status    = 'not_activated';
+			$userModel->activation_code      = '0xDEADFEED';
+			
+			$formModel->user_name             = $formModel->email;
+				
+						
+			$uploadedFile = CUploadedFile::getInstance($formModel,'picture');
+			
+			if ($userModel->validate() && $formModel->validate()) {
+			    
+			    if($userModel->save())
+			    {
+			        $imageFileName = 'user-'.$businessModel->business_id.'-'.$uploadedFile->name;
+			        $imagePath = $this->imagesDirPath.DIRECTORY_SEPARATOR.$imageFileName;
+			    
+			        if(!empty($uploadedFile))  // check if uploaded file is set or not
+			        {
+			            $uploadedFile->saveAs($imagePath);
+			            $userModel->image = $imageFileName;
+			             
+			            $this->createThumbnail($imageFileName);
+			             
+			            $userModel->save();
+			        }
+			         
+			        $this->redirect(array('index'));
+			         
+			    }
+			    else {
+			      print_r($userModel);exit;
+			        Yii::app()->user->setFlash('error', "Error creating a business record.'");
+			    }
+			    
+			}
+			else
+			{
+			    print_r($userModel);
+			    print_r($formModel);exit;
+			exit;
+			     
+			}
+
+		}
+		
+		$this->render('user_profile',array('model'=>$formModel));
+		
+	}
+	
+}
---
+++ protected/modules/webuser/models/ProfileForm.php
@@ -0,0 +1,177 @@
+<?php
+
+/**
+ * ProfileForm class.
+ * ProfileForm is the data structure for keeping
+ * password recovery form data. It is used by the 'recovery' action of 'DefaultController'.
+ */
+class ProfileForm extends CFormModel
+{
+	public $user_name;
+	public $email;
+	public $first_name;
+	public $last_name;
+	public $picture;
+	public $removePicture;
+	public $password;
+	public $confirm_password;
+	public $places_want_to_visit;
+
+	/**
+	 * @var IdentityInterface cached object returned by @see getIdentity()
+	 */
+	private $_identity;
+	/**
+	 * @var array Picture upload validation rules.
+	 */
+	private $_pictureUploadRules;
+
+	/**
+	 * Returns rules for picture upload or an empty array if they are not set.
+	 * @return array
+	 */
+	public function getPictureUploadRules()
+	{
+		return $this->_pictureUploadRules === null ? array() : $this->_pictureUploadRules;
+	}
+
+	/**
+	 * Sets rules to validate uploaded picture. Rules should NOT contain attribute name as this method adds it.
+	 * @param array $rules
+	 */
+	public function setPictureUploadRules($rules)
+	{
+		$this->_pictureUploadRules = array();
+		if (!is_array($rules))
+			return;
+		foreach($rules as $rule) {
+			$this->_pictureUploadRules[] = array_merge(array('picture'), $rule);
+		}
+	}
+
+	/**
+	 * Declares the validation rules.
+	 */
+	public function rules()
+	{
+ return array(
+			array('user_name, email, first_name, last_name, removePicture', 'filter', 'filter'=>'trim'),
+			array('user_name, email, first_name, last_name, removePicture', 'default', 'setOnEmpty'=>true, 'value' => null),
+
+			array('user_name, email', 'required'),
+			array('user_name, email', 'uniqueIdentity'),
+			array('email', 'email'),
+			array('removePicture', 'boolean'),
+			array('password', 'validCurrentPassword', 'except'=>'register'),
+            array('confirm_password', 'validCurrentPassword', 'except'=>'register'),
+		);
+	}
+
+	/**
+	 * Declares attribute labels.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'user_name'		=> Yii::t('UsrModule.usr','Username'),
+			'email'			=> Yii::t('UsrModule.usr','Email'),
+			'first_name'		=> Yii::t('UsrModule.usr','First name'),
+			'last_name'		=> Yii::t('UsrModule.usr','Last name'),
+			'picture'		=> Yii::t('UsrModule.usr','Profile picture'),
+			'removePicture'	=> Yii::t('UsrModule.usr','Remove picture'),
+			'password'		=> Yii::t('UsrModule.usr','Current password'),
+		    'confirm_password'		=> Yii::t('UsrModule.usr','Confirm password'),
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getIdentity()
+	{
+// 		if($this->_identity===null) {
+// 			$userIdentityClass = $this->userIdentityClass;
+// 			if ($this->scenario == 'register') {
+// 				$this->_identity = new $userIdentityClass(null, null);
+// 			} else {
+// 				$this->_identity = $userIdentityClass::find(array('id'=>Yii::app()->user->getId()));
+// 			}
+// 			if ($this->_identity !== null && !($this->_identity instanceof IEditableIdentity)) {
+// 				throw new CException(Yii::t('UsrModule.usr','The {class} class must implement the {interface} interface.',array('{class}'=>get_class($this->_identity),'{interface}'=>'IEditableIdentity')));
+// 			}
+// 		}
+// 		return $this->_identity;
+	}
+
+	public function setIdentity($identity)
+	{
+// 		$this->_identity = $identity;
+	}
+
+	public function uniqueIdentity($attribute,$params)
+	{
+		if($this->hasErrors()) {
+			return;
+		}
+// 		$userIdentityClass = $this->userIdentityClass;
+// 		$existingIdentity = $userIdentityClass::find(array($attribute => $this->$attribute));
+// 		if ($existingIdentity !== null && (($identity=$this->getIdentity()) !== null && $existingIdentity->getId() != $identity->getId())) {
+// 			$this->addError($attribute,Yii::t('UsrModule.usr','{attribute} has already been used by another user.', array('{attribute}'=>$this->$attribute)));
+// 			return false;
+// 		}
+		return true;
+	}
+
+	/**
+	 * A valid current password is required only when changing email.
+	 */
+	public function validCurrentPassword($attribute,$params)
+	{
+		if($this->hasErrors()) {
+			return;
+		}
+// 		if (($identity=$this->getIdentity()) === null) {
+// 			throw new CException('Current user has not been found in the database.');
+// 		}
+// 		if ($identity->getEmail() === $this->email) {
+// 			return true;
+// 		}
+// 		$identity->password = $this->$attribute;
+// 		if(!$identity->authenticate()) {
+// 			$this->addError($attribute, Yii::t('UsrModule.usr', 'Changing email address requires providing the current password.'));
+// 			return false;
+// 		}
+		return true;
+	}
+
+	/**
+	 * Logs in the user using the given user_name.
+	 * @return boolean whether login is successful
+	 */
+	public function login()
+	{
+// 		$identity = $this->getIdentity();
+
+// 		return Yii::app()->user->login($identity,0);
+	}
+
+	/**
+	 * Updates the identity with this models attributes and saves it.
+	 * @param CUserIdentity $identity
+	 * @return boolean whether saving is successful
+	 */
+	public function save()
+	{
+// 		if (($identity=$this->getIdentity()) === null)
+// 			return false;
+
+// 		$identity->setAttributes($this->getAttributes());
+// 		if ($identity->save(Yii::app()->controller->module->requireVerifiedEmail)) {
+// 			if ((!($this->picture instanceof CUploadedFile) || $identity->savePicture($this->picture)) && (!$this->removePicture || $identity->removePicture())) {
+// 				$this->_identity = $identity;
+// 				return true;
+// 			}
+// 		}
+		return false;
+	}
+}
