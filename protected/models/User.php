f3565a334e88bf4720b88d54f739b46b9856b9e7
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-03-29 17:42:23 +0200

    Changed user/beforeSave() to encrypt password when user is registering

--- protected/models/User.php
+++ protected/models/User.php
@@ -391,6 +391,7 @@ class User extends CActiveRecord
         }
         
         if ( ($this->scenario == self::SCENARIO_CHANGE_PASSWORD) ||
+             ($this->scenario == self::SCENARIO_REGISTER) ||
              ($this->scenario == 'insert') ||
              ($this->scenario == 'update')
            )
