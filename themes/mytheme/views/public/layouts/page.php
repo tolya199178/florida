8c0bc6b1727286d97ef81e87d0cf29e6b051969c
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-03-31 15:47:27 +0200

    Show login screen for not logged in users only. Hide for logged in users

--- themes/mytheme/views/public/layouts/page.php
+++ themes/mytheme/views/public/layouts/page.php
@@ -85,35 +85,8 @@ body {
                             <li><a href="#">I want to...</a></li>
                         </ul></li>
                 </ul>
-                
-                <?php
-                     $model = new LoginForm();
-                     
-                     $form=$this->beginWidget('CActiveForm', array(
-                    	'id'=>'frmLogin',
-                        'action' => Yii::app()->createUrl('webuser/account/login/'),
-                    	'enableAjaxValidation'=>false,
-                    	'enableClientValidation'=>true,
-    //                 	'clientOptions'=>array(
-    //                 		'validateOnSubmit'=>true,
-    //                 	),
-                    	'htmlOptions' => array('class' => 'navbar-form navbar-right'),
-                )); ?>
-                    <div class="form-group">
-                        <?php echo $form->textField($model,'fldUserName', array('class'=>'form-control', 'placeholder'=>"E-mail address",
-                                        'data-toggle' => "tooltip", "data-placement" => "bottom", "title"=>"Enter your email address or user name", "data-original-title"=>"Enter your email address or user name."			                    
-                                   )); ?>
-                    </div>
-                    <div class="form-group">
-                        <?php echo $form->passwordField($model,'fldPassword', array('class'=>'form-control', 'placeholder'=>"Password",
-                                        'data-toggle' => "tooltip", "data-placement" => "bottom", "title"=>"Enter your password", "data-original-title"=>"Enter your password."			                    
-                                   )); ?>
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                    &nbsp;&nbsp;&nbsp;<a
-                        href="<?php echo Yii::app()->createUrl('webuser/account/register/'); ?>">Not
-                        a member?</a>
-                <?php $this->endWidget(); ?>
+
+                <?php if(Yii::app()->user->isGuest) {$this->widget('UserLogin');} ?>
             </div>
             <!--/.navbar-collapse -->
         </div>
