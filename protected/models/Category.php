34a306135898d39075770e98ccff46a48fc0330e
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-04-03 16:59:45 +0200

    Created new model file Category using std gii

---
+++ protected/models/Category.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * This is the model class for table "{{category}}".
+ *
+ * The followings are the available columns in table '{{category}}':
+ * @property integer $category_id
+ * @property integer $parent_id
+ * @property string $category_name
+ * @property string $category_description
+ *
+ * The followings are the available model relations:
+ * @property Category $parent
+ * @property Category[] $categories
+ */
+class Category extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{category}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('category_name, category_description', 'required'),
+			array('parent_id', 'numerical', 'integerOnly'=>true),
+			array('category_name', 'length', 'max'=>128),
+			array('category_description', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('category_id, parent_id, category_name, category_description', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'parent' => array(self::BELONGS_TO, 'Category', 'parent_id'),
+			'categories' => array(self::HAS_MANY, 'Category', 'parent_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'category_id' => 'Category',
+			'parent_id' => 'Parent',
+			'category_name' => 'Category Name',
+			'category_description' => 'Category Description',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('category_id',$this->category_id);
+		$criteria->compare('parent_id',$this->parent_id);
+		$criteria->compare('category_name',$this->category_name,true);
+		$criteria->compare('category_description',$this->category_description,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Category the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
