a2eaf6675f82747e895b680508ba3f3fd10138b4
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-04-07 09:53:23 +0200

    Customised new model SubscribedBusiness and changed Business and User for new relations

--- protected/models/SubscribedBusiness.php
+++ protected/models/SubscribedBusiness.php
@@ -12,89 +12,117 @@
  * @property Business $business
  * @property User $user
  */
+
+/**
+ * User activerecord model class provides a mechanism to keep data and their
+ * ...relevant business rules. A model instant represents a single database row.
+ * ...
+ * ...Usage:
+ * ...   $subscription = SubscribedBusiness::model()
+ * ...or
+ * ...   $subscription = new SubscribedBusiness;
+ * ...or
+ * ...   $subscription = new SubscribedBusiness($scenario);
+ *
+ * @package   Components
+ * @author    Pradesh <pradesh@datacraft.co.za>
+ * @copyright 2014 florida.com
+ * @package Components
+ * @version 1.0
+ */
 class SubscribedBusiness extends CActiveRecord
 {
-	/**
-	 * @return string the associated database table name
-	 */
+
+    /**
+     * Get database table name associated with the model.
+     *
+     * @param <none> <none>
+     *
+     * @return string the associated database table name
+     * @access public
+     */
 	public function tableName()
 	{
 		return '{{subscribed_business}}';
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
+    /**
+     * Set rules for validation of model attributes. Each attribute is listed with its
+     * ...associated rules. All attributes listed in the rules set forms a set of 'safe'
+     * ...attributes that allow it to be used in massive assignment.
+     *
+     * @param <none> <none>
+     *
+     * @return array validation rules for model attributes.
+     * @access public
+     */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
+
 		return array(
-			array('user_id, business_id', 'required'),
-			array('user_id, business_id', 'numerical', 'integerOnly'=>true),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('subscribed_business_id, user_id, business_id', 'safe', 'on'=>'search'),
+			array('user_id, business_id',        'required'),
+			array('user_id, business_id',        'numerical', 'integerOnly'=>true),
+
+			array('user_id, business_id',        'safe', 'on'=>'search'),
 		);
 	}
 
-	/**
-	 * @return array relational rules.
-	 */
+    /**
+     * Set rules for the relation of this record model to other record models.
+     *
+     * @param <none> <none>
+     *
+     * @return array relational rules.
+     * @access public
+     */
 	public function relations()
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'business' => array(self::BELONGS_TO, 'Business', 'business_id'),
-			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+			'business'   => array(self::BELONGS_TO, 'Business', 'business_id'),
+			'user'       => array(self::BELONGS_TO, 'User', 'user_id'),
 		);
 	}
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'subscribed_business_id' => 'Subscribed Business',
-			'user_id' => 'User',
-			'business_id' => 'Business',
-		);
-	}
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @param <none> <none>
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     *         ...based on the search/filter conditions.
+     * @access public
+     */
 	public function search()
 	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
 
 		$criteria=new CDbCriteria;
 
-		$criteria->compare('subscribed_business_id',$this->subscribed_business_id);
-		$criteria->compare('user_id',$this->user_id);
-		$criteria->compare('business_id',$this->business_id);
+		$criteria->compare('user_id',     $this->user_id);
+		$criteria->compare('business_id', $this->business_id);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
 		));
 	}
 
+
 	/**
 	 * Returns the static model of the specified AR class.
 	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 *
 	 * @param string $className active record class name.
 	 * @return SubscribedBusiness the static model class
+	 *
+	 * @access public
 	 */
 	public static function model($className=__CLASS__)
 	{
