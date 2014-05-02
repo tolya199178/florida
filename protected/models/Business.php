be427f953e5e79760967b42a08f2ae6f6dba973e
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-05-02 10:03:40 +0200

    Set user id to special case 1 for non-logged in user and command line apps

--- protected/models/Business.php
+++ protected/models/Business.php
@@ -340,14 +340,14 @@ class Business extends CActiveRecord
         // /////////////////////////////////////////////////////////////////
         if ($this->isNewRecord) {
             $this->created_time = new CDbExpression('NOW()');
-            $this->created_by   = Yii::app()->user->id;
+            $this->created_by   = (Yii::app() instanceof CConsoleApplication || (!(Yii::app()->user->id)) ? 1 : Yii::app()->user->id);
         }
 
         // /////////////////////////////////////////////////////////////////
         // The modified log details is set for record creation and update
         // /////////////////////////////////////////////////////////////////
         $this->modified_time = new CDbExpression('NOW()');
-        $this->modified_by   = Yii::app()->user->id;
+        $this->modified_by   = (Yii::app() instanceof CConsoleApplication || (!(Yii::app()->user->id)) ? 1 : Yii::app()->user->id);
 
 	    return parent::beforeSave();
 	}
