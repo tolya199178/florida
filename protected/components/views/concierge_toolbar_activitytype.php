1e7ad7b05def64af6959c7eedd8870c139d475bb
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-05-19 14:23:05 +0200

    Minor tweaks to concierge UI

--- protected/components/views/concierge_toolbar_activitytype.php
+++ protected/components/views/concierge_toolbar_activitytype.php
@@ -1,8 +1,16 @@
 <?php       if (count($listCategorySearch) > 0) { ?>
-                <div>Popular Types</div>
-                    <div class="btn-toolbar" role="toolbar" style="margin: 0;">
-<?php               foreach ($listCategorySearch as $searchLogEntry) { ?>
-                        <button type="button" class="btn btn-sm btn-warning concierge_activitytype_tag"><?php echo $searchLogEntry['keyword']; ?></button>
-<?php               } ?>
-                </div>
+    <div class="row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col">
+            <div>Popular Types</div>
+        </div>
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col">
+            <div class="btn-toolbar" role="toolbar" style="margin: 0;padding-bottom:1px;">
+<?php       foreach ($listCategorySearch as $searchLogEntry) { ?>
+                <button type="button" class="btn btn-sm btn-warning concierge_activitytype_tag"><?php echo $searchLogEntry['keyword']; ?></button>
+<?php       } ?>
+            </div>
+
+        </div>
+
+    </div>
 <?php       } ?>
