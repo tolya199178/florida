1249dfbf2bf4a9a67c7e742f10436691f0afae47
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-05-19 11:40:31 +0200

    Fixes to backend event screens and controller due to old or incorrect field names

--- themes/mytheme/views/backend/event/details.php
+++ themes/mytheme/views/backend/event/details.php
@@ -13,7 +13,7 @@
 </style>
 
     <h3>Update Event : <?php echo $model->event_title; ?></h3>
-        
+
         <!-- todo: jquery order loading issue where setting enableAjaxValidation=true -->
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'event-details-form',
@@ -26,27 +26,27 @@
 
     <?php if($model->hasErrors()) {  ?>
         <div class="alert alert-danger">
-        	<?php echo $form->errorSummary($model); ?>	
+        	<?php echo $form->errorSummary($model); ?>
         </div>
     <?php } ?>
-    
+
     <?php echo $form->hiddenField($model,'event_id'); ?>
-    
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_title',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_title',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_title'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
-        <div class="form-group">	
+        <div class="form-group">
             <?php echo $form->labelEx($model,'event_start_date',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
-                
+
                 <?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
                     'model' => $model,
                     'attribute' => 'event_start_date',
@@ -64,24 +64,24 @@
                 ?>
                 <?php echo $form->error($model,'event_start_date'); ?>
             </div>
-        </div>	
+        </div>
     </div>
-    
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_start_time',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_start_time',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_start_time'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-    
+
 	<div class="row">
-        <div class="form-group">	
+        <div class="form-group">
             <?php echo $form->labelEx($model,'event_end_date',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
-                
+
                 <?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
                     'model' => $model,
                     'attribute' => 'event_end_date',
@@ -99,134 +99,134 @@
                 ?>
                 <?php echo $form->error($model,'event_end_date'); ?>
             </div>
-        </div>	
+        </div>
     </div>
-    
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_end_time',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_end_time',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_end_time'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-    
+
 	<div class="row">
         <div class="form-group">
         TODO: read from category table
             <?php echo $form->labelEx($model,'event_category_id',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
-                <?php echo $form->dropDownList($model,'event_category_id', CHtml::listData(City::model()->findAll(), 'city_id', 'city_name')); ?> 
+                <?php echo $form->dropDownList($model,'event_category_id', CHtml::listData(City::model()->findAll(), 'city_id', 'city_name')); ?>
                 <?php echo $form->error($model,'event_category_id'); ?>
-                <!--  todo: styling for dropdown -->  
+                <!--  todo: styling for dropdown -->
             </div>
-        </div>	
+        </div>
 	</div>
 
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_business_id',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
-                <?php echo $form->dropDownList($model,'event_business_id', CHtml::listData(Business::model()->findAll(), 'city_id', 'city_name')); ?> 
+                <?php echo $form->dropDownList($model,'event_business_id', CHtml::listData(Business::model()->findAll(), 'business_id', 'business_name')); ?>
                 <?php echo $form->error($model,'event_business_id'); ?>
-                <!--  todo: styling for dropdown -->  
+                <!--  todo: styling for dropdown -->
             </div>
-        </div>	
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_tag',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_tag',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_tag'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_views',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_views',array('class'=>"form-control", 'readonly' => 'readonly')); ?>
                 <?php echo $form->error($model,'event_views'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'cost',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'cost',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'cost'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_frequency',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_frequency',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_frequency'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_address1',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textArea($model,'event_address1',array('class'=>"form-control", 'rows' => 4, 'cols' => 300)); ?>
                 <?php echo $form->error($model,'event_address1'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_address2',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textArea($model,'event_address2',array('class'=>"form-control",  'rows' => 4, 'cols' => 300)); ?>
                 <?php echo $form->error($model,'event_address2'); ?>
-            </div>        
-        </div>	
-	</div>	
-		
+            </div>
+        </div>
+	</div>
+
 	<!--  Add a city selection dropdown -->
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_city_id',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
-                <?php echo $form->dropDownList($model,'event_city_id', CHtml::listData(City::model()->findAll(), 'city_id', 'city_name')); ?> 
+                <?php echo $form->dropDownList($model,'event_city_id', CHtml::listData(City::model()->findAll(), 'city_id', 'city_name')); ?>
                 <?php echo $form->error($model,'event_city_id'); ?>
-                <!--  todo: styling for dropdown -->  
+                <!--  todo: styling for dropdown -->
             </div>
-        </div>	
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_street',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_street',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_street'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_phone_no',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'event_phone_no',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_phone_no'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_latitude',array('class'=>"col-sm-2 control-label")); ?>
@@ -235,40 +235,40 @@
                 <?php echo $form->error($model,'event_latitude'); ?>
                 <?php echo $form->textField($model,'event_longitude',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_longitude'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'is_featured',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
-                <?php echo $form->checkBox($model,'is_featured', array('value' => 'Y', 'uncheckValue'=>'N','class'=>"form-control")); ?>            
+                <?php echo $form->checkBox($model,'is_featured', array('value' => 'Y', 'uncheckValue'=>'N','class'=>"form-control")); ?>
                 <?php echo $form->error($model,'is_featured'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_show_map',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->checkBox($model,'event_show_map', array('value' => 'Y', 'uncheckValue'=>'N','class'=>"form-control")); ?>
                 <?php echo $form->error($model,'event_show_map'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'is_popular',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->checkBox($model,'is_popular', array('value' => 'Y', 'uncheckValue'=>'N','class'=>"form-control")); ?>
                 <?php echo $form->error($model,'is_popular'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_type',array('class'=>"col-sm-2 control-label")); ?>
@@ -279,10 +279,10 @@
                                                array('prompt'=>'Select Event Type')
                 );?>
                 <?php echo $form->error($model,'event_type'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'event_status',array('class'=>"col-sm-2 control-label")); ?>
@@ -293,30 +293,30 @@
                                                array('prompt'=>'Select Status')
                 );?>
                 <?php echo $form->error($model,'event_status'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'created_time',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo $form->textField($model,'created_time',array('class'=>"form-control", 'readonly'=>'readonly')); ?>
                 <?php echo $form->error($model,'created_time'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 	<div class="row">
         <div class="form-group">
             <?php echo $form->labelEx($model,'fldUploadImage',array('class'=>"col-sm-2 control-label")); ?>
             <div class="col-sm-4">
                 <?php echo CHtml::activeFileField($model,'fldUploadImage',array('class'=>"form-control")); ?>
                 <?php echo $form->error($model,'fldUploadImage'); ?>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
-	
+
 <?php if (!empty($model->event_photo)) { ?>
 	<div class="row">
         <div class="form-group">
@@ -327,13 +327,13 @@
                                             "Image",
                                             array('width'=>150, 'height'=>150))); ?>
                 </div>
-            </div>        
-        </div>	
+            </div>
+        </div>
 	</div>
 <?php } ?>
 
 
-	
+
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class'=>"btn btn-default")); ?>
 	</div>
--- themes/mytheme/views/backend/event/list.php
+++ themes/mytheme/views/backend/event/list.php
@@ -5,21 +5,21 @@
     $cs->registerScriptFile(Yii::app()->theme->baseUrl . '/resources/libraries/DataTables 1.10.0-beta.2/media/js/jquery.dataTables.js', CClientScript::POS_END);
 
 ?>
-    
+
 <style type="text/css">
     .dataTables_filter {
     	display: none;
     }
-    
+
     #popover-content {
     	margin-right: -800px;
     	max-width: 800px;
     	width: 800px;
     }
 </style>
-    
-    
-    
+
+
+
     <nav class="navbar navbar-default" role="navigation">
         <div class="navbar-header">
             <a class="navbar-brand" href="#">Manage Events</a>
@@ -37,50 +37,49 @@
                     <input type="text" class="form-control" id="searchbox"
                         placeholder="Search">
                 </div>
-    
+
                 <a data-placement="bottom" data-toggle="popover" data-title=""
                     data-container="body" type="button" data-html="true" href="#"
                     id="login" class="btn btn-primary">Advanced Search</a>
                 <div id="popover-content" class="hide">
-    
+
                     some content
                     <p id="renderingEngineFilter"></p>
                     <p id="browserFilter"></p>
                     <p id="platformsFilter"></p>
                     <p id="engineVersionFilter"></p>
                     <p id="cssGradeFilter"></p>
-    
+
                 </div>
-    
+
             </div>
-    
-    
+
+
         </div>
     </nav>
-    
+
     <table id="event_table" class="table table-striped table-bordered"
         cellspacing="0" width="100%">
         <thead>
             <tr>
                 <th>Event ID</th>
                 <th>Event Title</th>
-                <th>Created By</th>
-                <th>Business Name</th>
-                <th>Category</th>
-                <th>Date</th>
-                <th>Status</th>
+                <th>Description</th>
+                <th>Event Type</th>
+                <th>Start Date</th>
+                <th>End Date</th>
                 <th>&nbsp;</th>
                 <th>&nbsp;</th>
-    
+
             </tr>
         </thead>
     </table>
-    
+
 <?php
     $data_url = Yii::app()->createUrl('event/listjson');
     $edit_url = Yii::app()->createUrl('event/edit');
     $delete_url = Yii::app()->createUrl('event/delete');
-    
+
 $script = <<<EOD
     var table = $('#event_table').DataTable({
             // Setup for Bootstrap support.
@@ -89,7 +88,7 @@ $script = <<<EOD
                 sLengthMenu: '_MENU_ records per page'
             },
             // bFilter: false,
-    
+
             'bProcessing': true,
             'bServerSide': true,
              'sPaginationType': 'full_numbers',
@@ -97,11 +96,11 @@ $script = <<<EOD
                 "url": '{$data_url}',
                 "type": "POST"
             },
-    
+
             'columnDefs': [
                 // These columns not visible
                 { 'visible': false,  'targets': [ 0 ] },
-                
+
                 // In row delete button
                 {
                     "targets": [-1],
@@ -112,7 +111,7 @@ $script = <<<EOD
                         return editurl;
                     },
                 },
-                
+
                 // In row edit button
                 {
                     "targets": [-2],
@@ -126,53 +125,53 @@ $script = <<<EOD
 
             ]
         });
-    
+
         $('#event_table tbody').on( 'click', 'button', function () {
-          
+
             var className = this.className;
-          
+
             var data = table.row( $(this).parents('tr') ).data();
-          
+
             var result = window.confirm("Do you really want to Delete the event record for " + data[1] + "?");
             if (result == false) {
                 e.preventDefault();
                 return false;
             }
             else {
-            
+
                 $.ajax({
                     type: 'POST',
                     data: { "event_id": data[0] },
                     dataType: 'json',
                     url: '{$delete_url}',
                     success: function (data) {
-                    
+
                         if (data.result == 'success') {
                             // Refresh the table
                             // var oTable = $('#tModuleListing').dataTable();
-                            table.draw();   
+                            table.draw();
                         }
                         else {
                             alert('Failed to Delete item - ' + data.message);
                         }
-                        
+
                     },
                     error: function () {
                         alert('Failed to Delete item - unexpected error');
-                        // todo: add proper error message 
+                        // todo: add proper error message
                     }
                 });
             }
-          
-    
-        
+
+
+
         return true;
-          
-          
+
+
           if (className == "editrow") {
             var data = table.row( $(this).parents('tr') ).data();
             // alert( data[0] +"'s salary is: "+ data[ 3 ] );
-        
+
             //  $('#myModal').modal('show');
             url = "{$edit_url}/event_id/" + data[0];
             $('#myModal').removeData();
@@ -180,34 +179,34 @@ $script = <<<EOD
                     remote : url
                 });
                 //$('#myModal').removeData();
-          
+
           }
           else {
                 var data = table.row( $(this).parents('tr') ).data();
           }
-          
+
         } );
-    
+
         // Search the table from the external search textbox. Limit to search string > 3 characters
         $('#searchbox').on( 'keyup', function () {
            if (($("#searchbox").val().length > 2) || ($("#searchbox").val().length == 0)) {
                 table.search( this.value ).draw();
            }
-                            
+
         } );
-                        
+
         $("[data-toggle=popover]").popover({
-            html: true, 
+            html: true,
         	content: function() {
                   return $('#popover-content').html();
                 }
         });
-    
-    
+
+
 EOD;
-    
+
     Yii::app()->clientScript->registerScript('register_script_name', $script, CClientScript::POS_READY);
-    
+
 ?>
-    
+
 
