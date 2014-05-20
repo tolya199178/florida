f22d315904da4fb3a008249625e24fd6c18666bf
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-05-20 17:56:50 +0200

    Changed display of event result view to be consistent with business result views

--- themes/mytheme/views/public/concierge/search_results_container.php
+++ themes/mytheme/views/public/concierge/search_results_container.php
@@ -1,9 +1,10 @@
     <div class="row">
 
+<?php   if (count($model) > 0) { ?>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			<div class="panel panel-default margin-top-10">
 				<div class="panel-heading">
-					<h3>Featured guesthouse destinations</h3>
+					<h3>Featured Business</h3>
 				</div>
 
 				<div class="row margin-top-10">
@@ -12,7 +13,7 @@
 
                         foreach ($model as $objBusiness)
                         {
-                            $this->renderPartial('result_business_entity', array('data'              => $objBusiness));
+                            $this->renderPartial('result_business_entity', array('data' => $objBusiness));
                         }
 
                     ?>
@@ -20,13 +21,35 @@
 				</div>
 			</div>
 		</div>
-    </div>
+<?php   }
+        else { ?>
+        <h4>No matching businesses found</h4>
+<?php   } ?>
 
-    <div class="row">
+<?php   if (count($listEvent) > 0) { ?>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="panel panel-default margin-top-10">
+				<div class="panel-heading">
+					<h3>Featured Events</h3>
+				</div>
 
-            <?php
+				<div class="row margin-top-10">
 
-                $this->renderPartial('result_event_details', array('data' => $listEvent));
+                    <?php
+                        // $this->renderPartial('result_event_details', array('data' => $listEvent));
 
-            ?>
+                        foreach ($listEvent as $eventItem)
+                        {
+                            $this->renderPartial('result_event_details', array('eventItem' => $eventItem));
+                            // $this->renderPartial('result_business_entity', array('data'              => $objBusiness));
+                        }
+                    ?>
+
+				</div>
+			</div>
+		</div>
+<?php   }
+        else { ?>
+        <h4>No matching events found</h4>
+<?php   } ?>
     </div>
