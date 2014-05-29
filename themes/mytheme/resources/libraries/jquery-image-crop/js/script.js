bab8beb840185cd9f964b1e08ed289546276aded
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-05-29 10:01:03 +0200

    Updated jquery crop due to conflict with Jquery 1.10- plugin used deprecated functions

--- themes/mytheme/resources/libraries/jquery-image-crop/js/script.js
+++ themes/mytheme/resources/libraries/jquery-image-crop/js/script.js
@@ -1,41 +1,92 @@
-/**
- *
- * Crop Image While Uploading With jQuery
- * 
- * Copyright 2013, Resalat Haque
- * http://www.w3bees.com/
- *
- */
+/*
+* Author : Ali Aboussebaba
+* Email : bewebdeveloper@gmail.com
+* Website : http://www.bewebdeveloper.com
+* Subject : Crop photo using PHP and jQuery
+*/
 
-// set info for cropping image using hidden fields
-function setInfo(i, e) {
-	$('#x').val(e.x1);
-	$('#y').val(e.y1);
-	$('#w').val(e.width);
-	$('#h').val(e.height);
+// the target size
+var TARGET_W = 600;
+var TARGET_H = 300;
+
+// show loader while uploading photo
+function submit_photo() {
+	// display the loading texte
+	$('#loading_progress').html('<img src="images/loader.gif"> Uploading your photo...');
 }
 
-$(document).ready(function() {
-	var p = $("#uploadPreview");
+// show_popup : show the popup
+function show_popup(id) {
+	// show the popup
+	$('#'+id).show();
+}
 
-	// prepare instant preview
-	$("#uploadImage").change(function(){
-		// fadeOut or hide preview
-		p.fadeOut();
+// close_popup : close the popup
+function close_popup(id) {
+	// hide the popup
+	$('#'+id).hide();
+}
 
-		// prepare HTML5 FileReader
-		var oFReader = new FileReader();
-		oFReader.readAsDataURL(document.getElementById("uploadImage").files[0]);
+// show_popup_crop : show the crop popup
+function show_popup_crop(url) {
+	// change the photo source
+	$('#cropbox').attr('src', url);
+	// destroy the Jcrop object to create a new one
+	try {
+		jcrop_api.destroy();
+	} catch (e) {
+		// object not defined
+	}
+	// Initialize the Jcrop using the TARGET_W and TARGET_H that initialized before
+    $('#cropbox').Jcrop({
+      aspectRatio: TARGET_W / TARGET_H,
+      setSelect:   [ 100, 100, TARGET_W, TARGET_H ],
+      onSelect: updateCoords
+    },function(){
+        jcrop_api = this;
+    });
 
-		oFReader.onload = function (oFREvent) {
-	   		p.attr('src', oFREvent.target.result).fadeIn();
-		};
+    // store the current uploaded photo url in a hidden input to use it later
+	$('#photo_url').val(url);
+	// hide and reset the upload popup
+	$('#popup_upload').hide();
+	$('#loading_progress').html('');
+	$('#photo').val('');
+
+	// show the crop popup
+	$('#popup_crop').show();
+}
+
+// crop_photo : 
+function crop_photo() {
+	var x_ = $('#x').val();
+	var y_ = $('#y').val();
+	var w_ = $('#w').val();
+	var h_ = $('#h').val();
+	var photo_url_ = $('#photo_url').val();
+
+	// hide thecrop  popup
+	$('#popup_crop').hide();
+
+	// display the loading texte
+	$('#photo_container').html('<img src="images/loader.gif"> Processing...');
+	// crop photo with a php file using ajax call
+	$.ajax({
+		url: 'crop_photo.php',
+		type: 'POST',
+		data: {x:x_, y:y_, w:w_, h:h_, photo_url:photo_url_, targ_w:TARGET_W, targ_h:TARGET_H},
+		success:function(data){
+			// display the croped photo
+			$('#photo_container').html(data);
+		}
 	});
+}
 
-	// implement imgAreaSelect plug in (http://odyniec.net/projects/imgareaselect/)
-	$('img#uploadPreview').imgAreaSelect({
-		// set crop ratio (optional)
-		aspectRatio: '1:1',
-		onSelectEnd: setInfo
-	});
-});
+// updateCoords : updates hidden input values after every crop selection
+function updateCoords(c) {
+	$('#x').val(c.x);
+	$('#y').val(c.y);
+	$('#w').val(c.w);
+	$('#h').val(c.h);
+}
+
