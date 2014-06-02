754331e4ecb07687cb1468f14820059c059ca1a1
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-06-02 16:16:17 +0200

    Corrected misplaced range parameter

--- protected/modules/dialogue/models/PostAnswer.php
+++ protected/modules/dialogue/models/PostAnswer.php
@@ -75,8 +75,8 @@ class PostAnswer extends CActiveRecord
 			array('user_id, question_id, title, alias, content',                 'required'),
 			array('user_id, question_id, votes, reply_to',                       'numerical', 'integerOnly'=>true),
 			array('title, alias',                                                'length', 'max'=>255),
-		    array('status',                                                      'in',
-            array('content, tags',                                               'length', 'max'=>4096),		                                                                         'range'=>array('Open','Closed', 'Published', 'Unublished')),
+		    array('status',                                                      'in','range'=>array('Open','Closed', 'Published', 'Unublished')),
+            array('content, tags',                                               'length', 'max'=>4096),
 	        array('entity_type',                                                 'in',
 		                                                                         'range'=>array('city', 'state', 'business', 'user', 'general', 'event')),
 
