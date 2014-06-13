a73502cc930fd1d87072a339da06b1cb59805582
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-06-13 14:43:49 +0200

    Changed links in site layout for change-password and logout menu items

--- themes/mytheme/views/public/layouts/front.php
+++ themes/mytheme/views/public/layouts/front.php
@@ -133,12 +133,12 @@
                         data-toggle="dropdown">Manage Your Account<b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li><a href="<?php echo Yii::app()->createUrl('webuser/account/manageprofile/'); ?>">Manage Profile</a></li>
-                            <li><a href="#changepassword">Change Password</a></li>
+                            <li><a href="<?php echo Yii::app()->createUrl('webuser/account/changepassword/'); ?>">Change Password</a></li>
                             <li class="divider"></li>
                             <li><a href="<?php echo Yii::app()->createUrl('myfriend/'); ?>">My friends</a></li>
 
                             <li class="divider"></li>
-                            <li><a href="#logout">Logout</a></li>
+                            <li><a href="<?php echo Yii::app()->createUrl('webuser/account/logout/'); ?>">Logout</a></li>
 
                         </ul>
                     <li><a href="#about">About</a></li>
