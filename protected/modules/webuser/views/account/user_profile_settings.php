0cad51fdc146421ad67cdec9ac894def286de0c1
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-06-20 00:27:39 +0200

    Added link for preview

--- protected/modules/webuser/views/account/user_profile_settings.php
+++ protected/modules/webuser/views/account/user_profile_settings.php
@@ -1,4 +1,51 @@
-    	<div class="row">
+<?php
+ $script = <<<EOD
+
+            $(document).on('click','#preview_profile_page', function(event){
+
+                // Get the view settings value
+                var settings = { my_info_permissions        : $("#ProfileForm_my_info_permissions"),
+                                 photos_permissions         : $("#ProfileForm_photos_permissions"),
+                                 friends_permissions        : $("#ProfileForm_friends_permissions"),
+                                 blogs_permissions          : $("#ProfileForm_blogs_permissions"),
+                                 travel_options_permissions : $("#ProfileForm_travel_options_permissions")
+                               }
+
+                event.preventDefault();
+
+                 var url         = $(this).attr('href');
+
+         		// process the form. Note that there is no data send as posts arguements.
+         		$.ajax({
+         			type 		: 'POST',
+         			url 		: url,
+         		    data 		: settings,
+         			dataType 	: 'json'
+         		})
+         		// using the done promise callback
+         		.done(function(data) {
+
+                     if (data.result == false)
+                     {
+                         return false;
+                     }
+
+                     $('#'+target).html(data.votes);
+
+
+         		});
+
+                return false;
+
+
+           });
+
+EOD;
+
+Yii::app()->clientScript->registerScript('register_script_name', $script, CClientScript::POS_READY);
+
+?>
+        <div class="row">
             <div class="form-group">
                 <?php echo $form->labelEx($model,'places_visited',array('class'=>"col-sm-2 control-label")); ?>
                 <div class="col-sm-4">
@@ -22,18 +69,6 @@
             </div>
         </div>
 
-
-    	<div class="row">
-            <div class="form-group">
-                <?php echo $form->labelEx($model,'my_info_permissions',array('class'=>"col-sm-2 control-label")); ?>
-                <div class="col-sm-4">
-                    <?php echo $form->dropDownList($model, 'my_info_permissions', User::listPermissions(), array('prompt'=>'Select Premission Level'));?>
-                    <?php echo $form->error($model,'my_info_permissions'); ?>
-                </div>
-            </div>
-    	</div>
-
-
     	<div class="row">
             <div class="form-group">
                 <?php echo $form->labelEx($model,'my_info_permissions',array('class'=>"col-sm-2 control-label")); ?>
@@ -48,7 +83,7 @@
             <div class="form-group">
                 <?php echo $form->labelEx($model,'photos_permissions',array('class'=>"col-sm-2 control-label")); ?>
                 <div class="col-sm-4">
-                    <?php echo $form->dropDownList($model, 'photos_permissions', User::listPermissions(), array('prompt'=>'Select Premission Level'));?>s
+                    <?php echo $form->dropDownList($model, 'photos_permissions', User::listPermissions(), array('prompt'=>'Select Premission Level'));?>
                     <?php echo $form->error($model,'photos_permissions'); ?>
                 </div>
             </div>
@@ -86,13 +121,12 @@
 
     	<div class="row">
             <div class="form-group">
-                <?php echo $form->labelEx($model,'picture',array('class'=>"col-sm-2 control-label")); ?>
+                &nbsp;
                 <div class="col-sm-4">
-                    <?php echo $form->textField($model,'picture',array('class'=>"form-control", 'readonly'=>'readonly')); ?>
-                    <?php echo $form->error($model,'picture'); ?>
+                    <a href="<?php echo Yii::app()->createUrl('webuser/account/previewprofile'); ?>" id='preview_profile_page'>Show Preview</a>
                 </div>
             </div>
-        </div>
+    	</div>
 
 
         <!-- end profile settings for type user -->
