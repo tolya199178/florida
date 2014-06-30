b8ba1bc86e527582d3fe9f9cedc604d9f3d46adb
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-06-30 13:39:46 +0200

    Changed URL to point to MyFriend module

--- protected/modules/dashboard/views/dashboard/components/friends_summary.php
+++ protected/modules/dashboard/views/dashboard/components/friends_summary.php
@@ -6,31 +6,31 @@
 
                             <li class="list-group-item text-right">
                                 <span class="pull-left">
-                                    <?php echo CHtml::link('<strong>My Friends</strong>', Yii::app()->createUrl('/dashboard/dashboard/show/component/allfriends/')); ?>
+                                    <?php echo CHtml::link('<strong>My Friends</strong>', Yii::app()->createUrl('/myfriend/myfriend/show/allfriends/')); ?>
                                 </span>
-                                <?php echo CHtml::link($myFriendsCount['allfriends'], Yii::app()->createUrl('/dashboard/dashboard/show/component/allfriends/')); ?>
+                                <?php echo CHtml::link($myFriendsCount['allfriends'], Yii::app()->createUrl('/myfriend/myfriend/show/allfriends/')); ?>
                             </li>
                             <li class="list-group-item text-right">
                                 <span class="pull-left">
-                                    <?php echo CHtml::link('<strong>Friends Online</strong>', Yii::app()->createUrl('/dashboard/dashboard/show/component/onlinefriends/')); ?>
+                                    <?php echo CHtml::link('<strong>Friends Online</strong>', Yii::app()->createUrl('/myfriend/myfriend/show/onlinefriends/')); ?>
                                 </span>
-                                <?php echo CHtml::link($myFriendsCount['onlinefriends'], Yii::app()->createUrl('/dashboard/dashboard/show/component/onlinefriends/')); ?>
+                                <?php echo CHtml::link($myFriendsCount['onlinefriends'], Yii::app()->createUrl('/myfriend/myfriend/show/onlinefriends/')); ?>
                             </li>
                             <li class="list-group-item text-right">
                                 <span class="pull-left">
-                                    <?php echo CHtml::link('<strong>Sent Friend Invitations</strong>', Yii::app()->createUrl('/dashboard/dashboard/show/component/onlinefriends/')); ?>
+                                    <?php echo CHtml::link('<strong>Sent Friend Invitations</strong>', Yii::app()->createUrl('/myfriend/myfriend/show/onlinefriends/')); ?>
                                 </span>
-                                <?php echo CHtml::link($myFriendsCount['sentfriendrequests'], Yii::app()->createUrl('/dashboard/dashboard/show/component/sentfriendrequests/')); ?>
+                                <?php echo CHtml::link($myFriendsCount['sentfriendrequests'], Yii::app()->createUrl('/myfriend/myfriend/show/sentfriendrequests/')); ?>
                             </li>
                             <li class="list-group-item text-right">
                                 <span class="pull-left">
-                                    <?php echo CHtml::link('<strong>Received Friend Invitations</strong>', Yii::app()->createUrl('/dashboard/dashboard/show/component/onlinefriends/')); ?>
+                                    <?php echo CHtml::link('<strong>Received Friend Invitations</strong>', Yii::app()->createUrl('/myfriend/myfriend/show/onlinefriends/')); ?>
                                 </span>
-                                <?php echo CHtml::link($myFriendsCount['receivedfriendrequests'], Yii::app()->createUrl('/dashboard/dashboard/show/component/receivedfriendrequests/')); ?>
+                                <?php echo CHtml::link($myFriendsCount['receivedfriendrequests'], Yii::app()->createUrl('/myfriend/myfriend/show/receivedfriendrequests/')); ?>
                             </li>
 
                             <li class="list-group-item text-center">
-                                <a class="btn btn-lg btn-success" href="<?php echo Yii::app()->createUrl('/dashboard/dashboard/show/component/allfriends/'); ?>">
+                                <a class="btn btn-lg btn-success" href="<?php echo Yii::app()->createUrl('/myfriend/myfriend/show/allfriends/'); ?>">
                                     <i class="glyphicon glyphicon-plus-sign"></i>
                                     Manage friends
                                 </a>
