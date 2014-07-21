b91c17975d89a02349be57538e9703e22f9fcb12
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-07-21 18:25:57 +0200

    Customised Coupon model - updated rules and added beforeSave(); modified Business model and modified User model for FK relations with coupon table

--- protected/models/Coupon.php
+++ protected/models/Coupon.php
@@ -76,16 +76,25 @@ class Coupon extends CActiveRecord
 	{
 
 		return array(
-			array('business_id, coupon_name, created_by, modified_by', 'required'),
-			array('business_id, redeemed_by, number_of_uses, created_by, modified_by', 'numerical', 'integerOnly'=>true),
-			array('coupon_name', 'length', 'max'=>250),
-			array('coupon_type', 'length', 'max'=>7),
-			array('coupon_photo', 'length', 'max'=>1024),
-			array('coupon_description', 'length', 'max'=>4096),
-			array('coupon_code', 'length', 'max'=>32),
-			array('printed', 'length', 'max'=>1),
-			array('cost', 'length', 'max'=>10),
-			array('coupon_expiry, terms, created_time, modified_time', 'safe'),
+
+		    // Mandatory rules
+			array('business_id, coupon_name, created_by, modified_by',   'required'),
+
+		    // Data types, sizes
+			array('business_id, redeemed_by, number_of_uses',            'numerical', 'integerOnly'=>true),
+			array('coupon_name', 'length',                               'max'=>250),
+		    array('coupon_photo', 'length',                              'max'=>1024),
+		    array('coupon_description, terms',                           'length', 'max'=>4096),
+
+		    array('coupon_code, coupon_expiry',                          'length', 'max'=>32),
+		    array('cost',                                                'length', 'max'=>10),
+
+		    // ranges
+		    array('coupon_type',
+		          'in', 'range'=>array('Unique','Generic'),'allowEmpty'=>false),
+		    array('printed',
+		          'in', 'range'=>array('Y','N'),'allowEmpty'=>false),
+
 
             // The following rule is used by search(). It only contains attributes that should be searched.
 			array('coupon_id, business_id, redeemed_by, coupon_name, number_of_uses, coupon_type, coupon_expiry, coupon_photo, coupon_description, coupon_code, terms, printed, cost, created_time, modified_time, created_by, modified_by', 'safe', 'on'=>'search'),
@@ -104,9 +113,9 @@ class Coupon extends CActiveRecord
 	{
 
 		return array(
-			'business'      => array(self::BELONGS_TO, 'Business', 'business_id'),
-			'createdBy'      => array(self::BELONGS_TO, 'User', 'created_by'),
-			'modifiedBy'      => array(self::BELONGS_TO, 'User', 'modified_by'),
+			'business'           => array(self::BELONGS_TO, 'Business', 'business_id'),
+			'createdBy'          => array(self::BELONGS_TO, 'User', 'created_by'),
+			'modifiedBy'         => array(self::BELONGS_TO, 'User', 'modified_by'),
 		);
 	}
 
@@ -124,23 +133,23 @@ class Coupon extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'coupon_id'      => 'Coupon',
-			'business_id'      => 'Business',
-			'redeemed_by'      => 'Redeemed By',
-			'coupon_name'      => 'Coupon Name',
-			'number_of_uses'      => 'Number Of Uses',
-			'coupon_type'      => 'Coupon Type',
-			'coupon_expiry'      => 'Coupon Expiry',
-			'coupon_photo'      => 'Coupon Photo',
-			'coupon_description'      => 'Coupon Description',
-			'coupon_code'      => 'Coupon Code',
-			'terms'      => 'Terms',
-			'printed'      => 'Printed',
-			'cost'      => 'Cost',
-			'created_time'      => 'Created Time',
-			'modified_time'      => 'Modified Time',
-			'created_by'      => 'Created By',
-			'modified_by'      => 'Modified By',
+			'coupon_id'              => 'Coupon',
+			'business_id'            => 'Business',
+			'redeemed_by'            => 'Redeemed By',
+			'coupon_name'            => 'Coupon Name',
+			'number_of_uses'         => 'Number Of Uses',
+			'coupon_type'            => 'Coupon Type',
+			'coupon_expiry'          => 'Coupon Expiry',
+			'coupon_photo'           => 'Coupon Photo',
+			'coupon_description'     => 'Coupon Description',
+			'coupon_code'            => 'Coupon Code',
+			'terms'                  => 'Terms',
+			'printed'                => 'Printed',
+			'cost'                   => 'Cost',
+			'created_time'           => 'Created Time',
+			'modified_time'          => 'Modified Time',
+			'created_by'             => 'Created By',
+			'modified_by'            => 'Modified By',
 		);
 	}
 
@@ -165,23 +174,23 @@ class Coupon extends CActiveRecord
 
 		$criteria=new CDbCriteria;
 
-		$criteria->compare('coupon_id',$this->coupon_id);
-		$criteria->compare('business_id',$this->business_id);
-		$criteria->compare('redeemed_by',$this->redeemed_by);
-		$criteria->compare('coupon_name',$this->coupon_name,true);
-		$criteria->compare('number_of_uses',$this->number_of_uses);
-		$criteria->compare('coupon_type',$this->coupon_type,true);
-		$criteria->compare('coupon_expiry',$this->coupon_expiry,true);
-		$criteria->compare('coupon_photo',$this->coupon_photo,true);
-		$criteria->compare('coupon_description',$this->coupon_description,true);
-		$criteria->compare('coupon_code',$this->coupon_code,true);
-		$criteria->compare('terms',$this->terms,true);
-		$criteria->compare('printed',$this->printed,true);
-		$criteria->compare('cost',$this->cost,true);
-		$criteria->compare('created_time',$this->created_time,true);
-		$criteria->compare('modified_time',$this->modified_time,true);
-		$criteria->compare('created_by',$this->created_by);
-		$criteria->compare('modified_by',$this->modified_by);
+		$criteria->compare('coupon_id',           $this->coupon_id);
+		$criteria->compare('business_id',         $this->business_id);
+		$criteria->compare('redeemed_by',         $this->redeemed_by);
+		$criteria->compare('coupon_name',         $this->coupon_name,true);
+		$criteria->compare('number_of_uses',      $this->number_of_uses);
+		$criteria->compare('coupon_type',         $this->coupon_type,true);
+		$criteria->compare('coupon_expiry',       $this->coupon_expiry,true);
+		$criteria->compare('coupon_photo',        $this->coupon_photo,true);
+		$criteria->compare('coupon_description',  $this->coupon_description,true);
+		$criteria->compare('coupon_code',         $this->coupon_code,true);
+		$criteria->compare('terms',               $this->terms,true);
+		$criteria->compare('printed',             $this->printed,true);
+		$criteria->compare('cost',                $this->cost,true);
+		$criteria->compare('created_time',        $this->created_time,true);
+		$criteria->compare('modified_time',       $this->modified_time,true);
+		$criteria->compare('created_by',          $this->created_by);
+		$criteria->compare('modified_by',         $this->modified_by);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
@@ -200,5 +209,34 @@ class Coupon extends CActiveRecord
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
+	}
+
+	/**
+	 * Runs just before the models save method is invoked. It provides a change to
+	 * ...further prepare the data for saving. The CActiveRecord (parent class)
+	 * ...beforeSave is called to process any raised events.
+	 *
+	 * @param <none> <none>
+	 * @return boolean the decision to continue the save or not.
+	 *
+	 * @access public
+	 */
+	public function beforeSave() {
+
+	    // /////////////////////////////////////////////////////////////////
+	    // Set the create time and user for new records
+	    // /////////////////////////////////////////////////////////////////
+	    if ($this->isNewRecord) {
+	        $this->created_time = new CDbExpression('NOW()');
+	        $this->created_by   = (Yii::app() instanceof CConsoleApplication || (!(Yii::app()->user->id)) ? 1 : Yii::app()->user->id);
+	    }
+
+	    // /////////////////////////////////////////////////////////////////
+	    // The modified log details is set for record creation and update
+	    // /////////////////////////////////////////////////////////////////
+	    $this->modified_time = new CDbExpression('NOW()');
+	    $this->modified_by   = (Yii::app() instanceof CConsoleApplication || (!(Yii::app()->user->id)) ? 1 : Yii::app()->user->id);
+
+	    return parent::beforeSave();
 	}
 }
