dc263435828c4cce7db1858dfe856e8757842f76
Pradesh Chanderpaul <pradesh@datacraft.co.za>
2014-08-17 15:19:39 +0200

    New version of load tn import data command ; this version loads venue data as well, and used the new services_utils functions to make requests

--- protected/commands/LoadTicketnetworkDataCommand.php
+++ protected/commands/LoadTicketnetworkDataCommand.php
@@ -28,8 +28,11 @@ require_once __DIR__ . '/../extensions/ticket-network/src/Exception/CustomExcept
 require_once __DIR__ . '/../extensions/ticket-network/ticketnetwork.class.php';
 require_once __DIR__ . '/../extensions/ticket-network/ticketnetwork_category.class.php';
 require_once __DIR__ . '/../extensions/ticket-network/ticketnetwork_state.class.php';
+require_once __DIR__ . '/../extensions/ticket-network/ticketnetwork_venue.class.php';
 
 require_once __DIR__ . '/../extensions/ticket-network/tnsample.php';
+
+require_once __DIR__ . 'services_utils.php';
 
 
 
@@ -64,6 +67,14 @@ class LoadTicketnetworkDataCommand extends CConsoleCommand
         if (!isset($userOptions['events']))
         {
             $userOptions['events'] = 'yes';
+        }
+        if (!isset($userOptions['venues']))
+        {
+            $userOptions['venues'] = 'yes';
+        }
+        if (!isset($userOptions['performers']))
+        {
+            $userOptions['performers'] = 'yes';
         }
         if (isset($userOptions['startdate']))
         {
@@ -182,7 +193,13 @@ class LoadTicketnetworkDataCommand extends CConsoleCommand
                         print_r($eventModel->attributes);
                         print_r($eventModel->getErrors());
 
+                        // Go ahead and read the next ine anyway.
+                        continue;
+
                     }
+
+                    // Getting here means a successful event record save. Save the venue details
+                    $this->getVenueDetails($eventDetails->VenueID);
 
                 }
 
@@ -217,6 +234,126 @@ EOD;
         echo $usage;
     }
 
+    /**
+     * Save the venue details
+     *
+     * @param integer venue_id The TN venue reference.
+     *
+     * @return boolean save result
+     * @access public
+     */
+    private function getVenueDetails($venueId)
+    {
+
+        // Get the venue details
+        $servicesHost       = "tnwebservices-test.ticketnetwork.com";
+        $servicesQuery      = "/tnwebservice/v3.1/tnwebservicestringinputs.asmx/GetVenue";
+
+        $servicesParameters = http_build_query(
+            array('websiteConfigID'  => WEB_CONF_ID,
+                  'venueID'          => $venueId));
+
+        $serviceRequest     = $servicesHost.$servicesQuery.'?'.$servicesParameters;
+
+        $venueResults       = sendGetRequest($serviceRequest);
+        $venueXmlResults    = simplexml_load_string($venueResults);
+        $allVenues          = simpleXMLToArray($venueXmlResults);
+        $venuDetails        = $allVenues['Venue'];
+
+        $modelTnVenue       = TnImportVenue::model()->findByAttributes(array('tn_ID'=>$venueId));
+
+        if ($modelTnVenue)      // Already exists
+        {
+            return true;
+        }
+        else                    // Not found
+        {
+            // Save the venue details
+            $modelTnVenue                   = new TnImportVenue;
+
+            $modelTnVenue->tn_ID            = ((!empty($venuDetails['ID']))?$venuDetails['ID']:null);
+            $modelTnVenue->tn_Name          = ((!empty($venuDetails['Name']))?$venuDetails['Name']:null);
+            $modelTnVenue->tn_Street1       = ((!empty($venuDetails['Street1']))?$venuDetails['Street1']:null);
+            $modelTnVenue->tn_Street2       = ((!empty($venuDetails['Street2']))?$venuDetails['Street2']:null);
+            $modelTnVenue->tn_StateProvince = ((!empty($venuDetails['StateProvince']))?$venuDetails['StateProvince']:null);
+            $modelTnVenue->tn_City          = ((!empty($venuDetails['City']))?$venuDetails['City']:null);
+            $modelTnVenue->tn_Country       = ((!empty($venuDetails['Country']))?$venuDetails['Country']:null);
+            $modelTnVenue->tn_BoxOfficePhone = ((!empty($venuDetails['BoxOfficePhone']))?$venuDetails['BoxOfficePhone']:null);
+            $modelTnVenue->tn_Directions    = ((!empty($venuDetails['Directions']))?$venuDetails['Directions']:null);
+            $modelTnVenue->tn_Parking       = ((!empty($venuDetails['Parking']))?$venuDetails['Parking']:null);
+            $modelTnVenue->tn_PublicTransportation = ((!empty($venuDetails['PublicTransportation']))?$venuDetails['PublicTransportation']:null);
+            $modelTnVenue->tn_URL           = ((!empty($venuDetails['URL']))?$venuDetails['URL']:null);
+            $modelTnVenue->tn_ZipCode       = ((!empty($venuDetails['ZipCode']))?$venuDetails['ZipCode']:null);
+            $modelTnVenue->tn_Capacity      = ((!empty($venuDetails['Capacity']))?$venuDetails['Capacity']:null);
+            $modelTnVenue->tn_ChildRules    = ((!empty($venuDetails['ChildRules']))?$venuDetails['ChildRules']:null);
+            $modelTnVenue->tn_Rules         = ((!empty($venuDetails['Rules']))?$venuDetails['Rules']:null);
+            $modelTnVenue->tn_Notes         = ((!empty($venuDetails['Notes']))?$venuDetails['Notes']:null);
+            $modelTnVenue->tn_NumberOfConfigurations = ((!empty($venuDetails['NumberOfConfigurations']))?$venuDetails['NumberOfConfigurations']:null);
+            $modelTnVenue->WillCall         = ((!empty($venuDetails['WillCall']))?$venuDetails['WillCall']:null);
+
+            if ($modelTnVenue->save())
+            {
+                // Saved the venue ok. Get the venue configuration file
+                $this->getVenueConfigurationDetails($venueId);
+            }
+            else
+            {
+                echo 'Error saving venue details for venue id : '.$venuDetails['ID'].'. Continuing despite error';
+            }
+        }
+
+
+    }
+
+    /**
+     * Save the venue details
+     *
+     * @param integer venue_id The TN venue reference.
+     *
+     * @return boolean save result
+     * @access public
+     */
+    private function getVenueConfigurationDetails($venueId)
+    {
+
+        // Get the venue details
+        $servicesHost       = "tnwebservices-test.ticketnetwork.com";
+        $servicesQuery      = "/tnwebservice/v3.1/tnwebservicestringinputs.asmx/GetVenueConfigurations";
+
+        $servicesParameters = http_build_query(
+            array('websiteConfigID'  => WEB_CONF_ID,
+                  'venueID'          => $venueId));
+
+        $serviceRequest         = $servicesHost.$servicesQuery.'?'.$servicesParameters;
+
+        $venueResults           = sendGetRequest($serviceRequest);
+        $venueXmlResults        = simplexml_load_string($venueResults);
+        $allVenueConfigurations = simpleXMLToArray($venueXmlResults);
+
+        $lstVenueConfigurations = $allVenueConfigurations['VenueConfiguration'];
+
+        foreach ($lstVenueConfigurations as $itemVenueConfiguration)
+        {
+            $modelVenueConfiguration                        = new TnImportVenueConfiguration;
+            $modelVenueConfiguration->tn_ID                 = ((!empty($itemVenueConfiguration['ID']))?$itemVenueConfiguration['ID']:null);
+            $modelVenueConfiguration->tn_Capacity           = ((!empty($itemVenueConfiguration['Capacity']))?$itemVenueConfiguration['Capacity']:null);
+            $modelVenueConfiguration->tn_MapSite            = ((!empty($itemVenueConfiguration['MapSite']))?$itemVenueConfiguration['MapSite']:null);
+            $modelVenueConfiguration->tn_MapURL             = ((!empty($itemVenueConfiguration['MapURL']))?$itemVenueConfiguration['MapURL']:null);
+            $modelVenueConfiguration->tn_VenueID            = ((!empty($itemVenueConfiguration['VenueID']))?$itemVenueConfiguration['VenueID']:null);
+            $modelVenueConfiguration->tn_TypeID             = ((!empty($itemVenueConfiguration['TypeID']))?$itemVenueConfiguration['TypeID']:null);
+            $modelVenueConfiguration->tn_TypeDescription    = ((!empty($itemVenueConfiguration['TypeDescription']))?$itemVenueConfiguration['TypeDescription']:null);
+
+            if ($modelVenueConfiguration->save() === false)
+            {
+                echo 'Error saving venue configuration details for venue id : '.$venueId.'. Continuing despite error';
+            }
+
+        }
+
+    }
+
 }
+
+
 
 ?>
